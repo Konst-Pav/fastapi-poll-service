b72efacdbf88e419aa76338d6cdb1d0a8a1e3917
kpavlov <k.pavlov.reg@yandex.ru>
2024-10-21 16:34:49 +0300

    Import Vote to models

--- poll-service-app/core/models/__init__.py
+++ poll-service-app/core/models/__init__.py
@@ -3,9 +3,11 @@ __all__ = (
     "Base",
     "Poll",
     "Option",
+    "Vote",
 )
 
 from .db_helper import db_helper
 from .base import Base
 from .poll import Poll
 from .option import Option
+from .vote import Vote
