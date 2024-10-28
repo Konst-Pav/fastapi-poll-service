ebbb34ba5e78f5099bb4cfe0c468048e3178640f
kpavlov <k.pavlov.reg@yandex.ru>
2024-10-28 16:03:12 +0300

    Import DatabaseHelper

--- poll-service-app/core/models/__init__.py
+++ poll-service-app/core/models/__init__.py
@@ -1,12 +1,13 @@
 __all__ = (
     "db_helper",
+    "DatabaseHelper",
     "Base",
     "Poll",
     "Option",
     "Vote",
 )
 
-from .db_helper import db_helper
+from .db_helper import db_helper, DatabaseHelper
 from .base import Base
 from .poll import Poll
 from .option import Option
